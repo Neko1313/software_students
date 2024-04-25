7e39072aac4ccf93858f732018bc24e2c8572746
KotenQuecotsu <serbianking@mail.ru>
2024-04-25 19:09:00 +1000

    task page template

--- page/test_view.py
+++ page/test_view.py
@@ -1,9 +1,40 @@
 import flet as ft
 import json
 
+tasks  = {
+    0: ft.Container(content=ft.Column([ft.Text('First Element') ])),
+    1: ft.Container(content=ft.Column([ft.Text('Second Element')]))
+}
 
-def test(page: ft.Page):
+
+
+
+def main(page: ft.Page):
+    
+    def change_tasks(number):
+        print(page.controls[0].controls[-1])
+        page.controls[0].controls.pop()
+        page.controls[0].controls.append(tasks[number])
+        page.update()
+
+    
     rail = ft.NavigationRail(destinations=[
-        ft.NavigationRailDestination(label='1'),
-        ft.NavigationRailDestination(label='2')
-    ])
+        ft.NavigationRailDestination(icon='home', icon_content=ft.Text('1', size=50),data=ft.Container(height=100)),
+        ft.NavigationRailDestination(icon_content=ft.Text('2',height=40), indicator_shape=ft.RoundedRectangleBorder),
+    ], on_change= lambda e: change_tasks(e.control.selected_index), )
+    
+    page.add(
+            ft.Row(
+                [
+                    rail,
+                    ft.VerticalDivider(width=1),
+                    ft.Column([ ft.Text("Body!")], alignment=ft.MainAxisAlignment.START, expand=True),
+                ],
+                expand=True,
+            )
+        )
+
+ft.app(target=main)
+    
+    
+    
